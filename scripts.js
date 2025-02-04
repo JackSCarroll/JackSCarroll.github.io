46d95febf6efda2e88f21d6ada4174aab5b69e96
JackSCarroll <64730336+JackSCarroll@users.noreply.github.com>
2025-02-04 18:46:34 +1100

    Add swup

--- scripts.js
+++ scripts.js
@@ -1,25 +1,42 @@
+ const swup = new Swup(); 
 
-//container.addEventListener('animationend', () => {
-//    container.classList.remove('apply-shake');
-//});
+ function initializeEventListeners() {
+     //
+     // Background Parallax Logic
+     //
+     const parallaxContainer = document.getElementById('parallax-container');
+     if (parallaxContainer) {
+         document.addEventListener('mousemove', (event) => {
+             const moveX = (event.clientX / window.innerWidth) * 20 - 10;
+             const moveY = (event.clientY / window.innerHeight) * 20 - 10;
+             parallaxContainer.style.backgroundPosition = `${moveX}px ${moveY}px`;
+         });
+     }
+ 
+     const html = document.documentElement;
+     html.classList.remove('is-animating');
+ 
+     //
+     // Mobile Hamburger Menu Logic
+     //
+     const hamMenu = document.getElementById('ham-menu');
+     const offScreenMenu = document.getElementById('off-screen-menu');
+     if (hamMenu && offScreenMenu) {
+         hamMenu.addEventListener('click', () => {
+             console.log('click');
+             hamMenu.classList.toggle('active');
+             offScreenMenu.classList.toggle('active');
+         });
+     }
+ }
+ 
+ document.addEventListener('DOMContentLoaded', () => {
+     initializeEventListeners();
+ });
+ 
+ // Reinitialize event listeners after Swup content replacement
+ swup.hooks.on('content:replace', () => {
+     initializeEventListeners();
+ });
 
-//
-// Main page console wiggle logic
-//
-const parallaxContainer = document.getElementById('parallax-container');
-document.addEventListener('mousemove', (event) => {
-    const moveX = (event.clientX / window.innerWidth) * 20 - 10;
-    const moveY = (event.clientY / window.innerHeight) * 20 - 10;
-    parallaxContainer.style.backgroundPosition = `${moveX}px ${moveY}px`;
-});
 
-
-//
-// Mobile Hambuger Menu Logic
-//
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-});
